2c89820998f97ffc5599cde5b57bb66ec68adbaf
Carlos Allen <allencarlosDev@gmail.com>
2021-07-09 15:23:32 -0400

    adding basic web structure

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Valorant blog</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+    <header class="header">
+        <div class="container logo-nav-container">
+            <a href="#" class="logo">LOGO VALORANT</a>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="#" class="">Home</a></li>
+                    <li><a href="#" class="">Patch Notes</a></li>
+                    <li><a href="#" class="">Highlights</a></li>
+                    <li><a href="#" class="">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main class="main">
+        <div class="container">
+            <h1>VALORANT BLOG</h1>
+            <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos laboriosam iste iusto ipsa,
+                ratione dignissimos in ipsum excepturi inventore mollitia! Recusandae, provident maiores inventore quas
+                corporis ratione. Libero dolorem excepturi ipsum eligendi, quod odit alias maxime est rem eos, in ut
+                provident consequatur magnam veniam ullam sit odio! Facere nihil minima aut ipsam, voluptatum saepe!
+                Nisi, maxime consequatur nam voluptates saepe, quisquam deleniti dolores sint obcaecati repellat labore
+                praesentium. Veniam recusandae tempore dicta cupiditate saepe magnam officia error numquam, incidunt
+                ipsa hic atque optio architecto aut vitae consectetur, quasi dignissimos minima? Voluptatum numquam
+                ipsam at ipsa, cupiditate nemo accusantium. Consequatur!</p>
+
+            <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos laboriosam iste iusto ipsa,
+                ratione dignissimos in ipsum excepturi inventore mollitia! Recusandae, provident maiores inventore quas
+                corporis ratione. Libero dolorem excepturi ipsum eligendi, quod odit alias maxime est rem eos, in ut
+                provident consequatur magnam veniam ullam sit odio! Facere nihil minima aut ipsam, voluptatum saepe!
+                Nisi, maxime consequatur nam voluptates saepe, quisquam deleniti dolores sint obcaecati repellat labore
+                praesentium. Veniam recusandae tempore dicta cupiditate saepe magnam officia error numquam, incidunt
+                ipsa hic atque optio architecto aut vitae consectetur, quasi dignissimos minima? Voluptatum numquam
+                ipsam at ipsa, cupiditate nemo accusantium. Consequatur!</p>
+
+            <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos laboriosam iste iusto ipsa,
+                ratione dignissimos in ipsum excepturi inventore mollitia! Recusandae, provident maiores inventore quas
+                corporis ratione. Libero dolorem excepturi ipsum eligendi, quod odit alias maxime est rem eos, in ut
+                provident consequatur magnam veniam ullam sit odio! Facere nihil minima aut ipsam, voluptatum saepe!
+                Nisi, maxime consequatur nam voluptates saepe, quisquam deleniti dolores sint obcaecati repellat labore
+                praesentium. Veniam recusandae tempore dicta cupiditate saepe magnam officia error numquam, incidunt
+                ipsa hic atque optio architecto aut vitae consectetur, quasi dignissimos minima? Voluptatum numquam
+                ipsam at ipsa, cupiditate nemo accusantium. Consequatur!</p>
+        </div>
+    </main>
+    <footer class="footer">
+
+    </footer>
+
+
 
 </body>
 
